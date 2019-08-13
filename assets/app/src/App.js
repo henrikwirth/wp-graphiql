cdfcd7f6bf4586d3038f324d67a9222311087225
henrikwirth <sirhenry91@gmail.com>
2019-08-13 10:30:01 +0200

    Adds initial implementation for GraphiQl-Explorer

--- assets/app/src/App.js
+++ assets/app/src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import GraphiQL from 'graphiql';
+import GraphiQLExplorer from "graphiql-explorer";
+import {buildClientSchema, getIntrospectionQuery, parse} from "graphql";
+
+// import { makeDefaultArg, getDefaultScalarArgValue } from "./CustomArgs";
 
 /**
  * Style the app
@@ -9,33 +13,165 @@ import './app.css';
 
 function graphQLFetcher(graphQLParams) {
 
-  /**
-   * Get the nonce & endpoint from the localized wpGraphiQLSettings object
-   * @type {null}
-   */
-  let nonce = ( window.wpGraphiQLSettings && window.wpGraphiQLSettings.nonce ) ? window.wpGraphiQLSettings.nonce : null;
-  let endpoint = ( window.wpGraphiQLSettings && window.wpGraphiQLSettings.graphqlEndpoint ) ? window.wpGraphiQLSettings.graphqlEndpoint : window.location.origin;
+	/**
+	 * Get the nonce & endpoint from the localized wpGraphiQLSettings object
+	 * @type {null}
+	 */
+	let nonce = (window.wpGraphiQLSettings && window.wpGraphiQLSettings.nonce) ? window.wpGraphiQLSettings.nonce : null;
+	let endpoint = (window.wpGraphiQLSettings && window.wpGraphiQLSettings.graphqlEndpoint) ? window.wpGraphiQLSettings.graphqlEndpoint : window.location.origin;
 
-  /**
-   * Fetch the WPGraphQL API
-   */
-  return fetch( endpoint, {
-    method: 'post',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-WP-Nonce': nonce
-    },
-    body: JSON.stringify(graphQLParams),
-  }).then(response => response.json());
+	/**
+	 * Fetch the WPGraphQL API
+	 */
+	return fetch(endpoint, {
+		method: 'post',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-WP-Nonce': nonce
+		},
+		body: JSON.stringify(graphQLParams),
+	}).then(response => response.json());
 
 }
 
+const DEFAULT_QUERY = `# shift-option/alt-click on a query below to jump to it in the explorer
+# option/alt-click on a field in the explorer to select all subfields
+query GET_PAGES {
+  pages {
+  	edges {
+  		node {
+  			title
+  		}
+  	}
+  }
+}
+`;
+
 const App = () => {
-  return (
-  	<div className="wrapper" >
-		<GraphiQL fetcher={graphQLFetcher} />
-	</div>
+
+	const [state, setState] = useState({
+		schema: null,
+		query: DEFAULT_QUERY,
+		explorerIsOpen: true
+	})
+
+
+	useEffect(() => {
+		graphQLFetcher({
+			query: getIntrospectionQuery()
+		}).then(result => {
+			const editor = GraphiQL.getQueryEditor();
+			editor.setOption("extraKeys", {
+				...(editor.options.extraKeys || {}),
+				"Shift-Alt-LeftClick": _handleInspectOperation
+			});
+
+			setState({schema: buildClientSchema(result.data)});
+		})
+	})
+
+	const _handleInspectOperation = (cm, mousePos) => {
+		const parsedQuery = parse(state.query || "");
+
+		if (!parsedQuery) {
+			console.error("Couldn't parse query document");
+			return null;
+		}
+
+		let token = cm.getTokenAt(mousePos);
+		let start = { line: mousePos.line, ch: token.start };
+		let end = { line: mousePos.line, ch: token.end };
+		let relevantMousePos = {
+			start: cm.indexFromPos(start),
+			end: cm.indexFromPos(end)
+		};
+
+		let position = relevantMousePos;
+
+		let def = parsedQuery.definitions.find(definition => {
+			if (!definition.loc) {
+				console.log("Missing location information for definition");
+				return false;
+			}
+
+			const { start, end } = definition.loc;
+			return start <= position.start && end >= position.end;
+		});
+
+		if (!def) {
+			console.error(
+				"Unable to find definition corresponding to mouse position"
+			);
+			return null;
+		}
+
+		let operationKind =
+			def.kind === "OperationDefinition"
+				? def.operation
+				: def.kind === "FragmentDefinition"
+				? "fragment"
+				: "unknown";
+
+		let operationName =
+			def.kind === "OperationDefinition" && !!def.name
+				? def.name.value
+				: def.kind === "FragmentDefinition" && !!def.name
+				? def.name.value
+				: "unknown";
+
+		let selector = `.graphiql-explorer-root #${operationKind}-${operationName}`;
+
+		let el = document.querySelector(selector);
+		el && el.scrollIntoView();
+	}
+
+	const _handleEditQuery = (query) => setState({ query })
+
+	const _handleToggleExplorer = () => {
+		setState({ explorerIsOpen: !state.explorerIsOpen })
+	}
+
+	return (
+		<div className="wrapper">
+			<GraphiQLExplorer
+				schema={state.schema}
+				query={state.query}
+				onEdit={_handleEditQuery}
+				onRunOperation={operationName =>
+					GraphiQL.handleRunQuery(operationName)
+				}
+				explorerIsOpen={state.explorerIsOpen}
+				onToggleExplorer={_handleToggleExplorer}
+				// getDefaultScalarArgValue={getDefaultScalarArgValue}
+				// makeDefaultArg={makeDefaultArg}
+			/>
+			<GraphiQL
+				ref={ref => (GraphiQL = ref)}
+				fetcher={graphQLFetcher}
+				schema={state.schema}
+				query={state.query}
+				onEditQuery={_handleEditQuery}
+			>
+				<GraphiQL.Toolbar>
+					<GraphiQL.Button
+						onClick={() => GraphiQL.handlePrettifyQuery()}
+						label="Prettify"
+						title="Prettify Query (Shift-Ctrl-P)"
+					/>
+					<GraphiQL.Button
+						onClick={() => GraphiQL.handleToggleHistory()}
+						label="History"
+						title="Show History"
+					/>
+					<GraphiQL.Button
+						onClick={_handleToggleExplorer}
+						label="Explorer"
+						title="Toggle Explorer"
+					/>
+				</GraphiQL.Toolbar>
+			</GraphiQL>
+		</div>
 	);
 }
 
